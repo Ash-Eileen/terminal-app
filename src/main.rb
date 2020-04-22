d607e1e3a8127546f03b43c1aba79916310bfcff
Ashley Smith <ash.mash.134@gmail.com>
2020-04-22 10:35:18 +1000

    Updated main.rb so text options can be tested on Mac and Windows

--- src/main.rb
+++ src/main.rb
@@ -1,26 +1,39 @@
 require 'random_word_generator'
+require 'tty'
+require 'colorize'
 
 require_relative './classes/hangman_image.rb'
 require_relative './classes/word_generator.rb'
 require_relative './classes/guess_checker.rb'
 
-loop do
-    system "clear"
-    word = WordGenerator.new("Multi!!")
-    word.generate_word
-    guess = GuessChecker.new(word.random_word)
-    guess.create_hidden_word
-    hangman = Hangman.new
+
+
+puts "██╗  ██╗ █████╗ ███╗   ██╗ ██████╗ ███╗   ███╗ █████╗ ███╗   ██╗".colorize(:light_green)
+puts "██║  ██║██╔══██╗████╗  ██║██╔════╝ ████╗ ████║██╔══██╗████╗  ██║".colorize(:light_red)
+puts "███████║███████║██╔██╗ ██║██║  ███╗██╔████╔██║███████║██╔██╗ ██║".colorize(:cyan)
+puts "██╔══██║██╔══██║██║╚██╗██║██║   ██║██║╚██╔╝██║██╔══██║██║╚██╗██║".colorize(:light_magenta)
+puts "██║  ██║██║  ██║██║ ╚████║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║ ╚████║".colorize(:light_blue)
+puts "╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝".colorize(:light_yellow)
+                                                            
+
+
+# loop do
+#     system "clear"
+#     word = WordGenerator.new("Multi!!")
+#     word.generate_word
+#     guess = GuessChecker.new(word.random_word)
+#     guess.create_hidden_word
+#     hangman = Hangman.new
     
-    sleep 1
-    system "clear"
-    until hangman.lost || guess.won
-        hangman.draw_hangman(word.random_word)
-        guess.display_hidden_and_attempts
-        guess.get_guess
-        guess.check_guess(hangman)
-        system "clear"
-        hangman.draw_hangman(word.random_word)
-        system "clear"
-    end
-end
+#     sleep 1
+#     system "clear"
+#     until hangman.lost || guess.won
+#         hangman.draw_hangman(word.random_word)
+#         guess.display_hidden_and_attempts
+#         guess.get_guess
+#         guess.check_guess(hangman)
+#         system "clear"
+#         hangman.draw_hangman(word.random_word)
+#         system "clear"
+#     end
+# end
