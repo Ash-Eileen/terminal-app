a31a6fd78fb78b2f887ca4de455df9968972ba66
Ashley Smith <ash.mash.134@gmail.com>
2020-04-22 17:24:31 +1000

    Added more cats

--- src/classes/word_generator.rb
+++ src/classes/word_generator.rb
@@ -9,7 +9,8 @@ class WordGenerator
     def generate_word(number_of_players)
         if number_of_players == 1
             system "clear"
-            puts "Hello Gamer. Let's play hangman. A word has been randomly generated for you to guess."
+            puts "Hello Gamer. Let's play hangman. \nA word has been randomly generated for you to guess."
+            puts Images::menu_cat.colorize(:light_blue)
             @word = RandomWordGenerator.word.upcase
             sleep 2
         else
@@ -19,6 +20,8 @@ class WordGenerator
                 puts "Please enter a word:"
                 @word = STDIN.gets.upcase.strip
                 puts "You have selected '#{word}'."
+                puts Images::menu_cat.colorize(:cyan)
+                sleep 2
                 raise ArgumentError, "Word has special characters or numbers" if @word.scan(/[^a-zA-Z]/).length != 0 || @word.length == 0
             rescue ArgumentError    
                 puts "No funny business buddy! Let's avoid special characters and numbers please.\n"
